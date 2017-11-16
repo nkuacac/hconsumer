e1fb2d21489ebf9a7fda7caa0d7d4325ac0e7a57
nkuacac <xiawencan@huawei.com>
2017-11-16 17:17:19 +0800

    Update HconsumerImpl.java

--- src/main/java/com/service/controller/HconsumerImpl.java
+++ src/main/java/com/service/controller/HconsumerImpl.java
@@ -3,63 +3,51 @@ package com.service.controller;
 import com.service.model.Person;
 
 import javax.ws.rs.core.MediaType;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 
-import io.servicecomb.provider.rest.common.RestSchema;
+import org.springframework.web.client.RestTemplate;
+import io.servicecomb.core.CseContext;
+import io.servicecomb.provider.springmvc.reference.RestTemplateBuilder;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.CseSpringCodegen", date = "2017-11-16T07:36:16.488Z")
 
-@RestSchema(schemaId = "hconsumer")
-@RequestMapping(path = "/hconsumer", produces = MediaType.APPLICATION_JSON)
 public class HconsumerImpl {
-
-    @Autowired
-    private HconsumerDelegate userHconsumerDelegate;
-
-
-    @RequestMapping(value = "/add",
-        produces = { "application/json" }, 
-        method = RequestMethod.GET)
-    public Integer add( @RequestParam(value = "a", required = true) Integer a, @RequestParam(value = "b", required = true) Integer b){
-
-        return userHconsumerDelegate.add(a, b);
+    public Integer add(Integer a, Integer b){
+        RestTemplate restTemplate = RestTemplateBuilder.create();
+        String microserviceName = "hprovider";
+        String prefix = "cse://" + microserviceName;
+        CseContext.getInstance().getConsumerProviderManager().setTransport(microserviceName, "rest");
+        return restTemplate.getForObject(prefix + "/hprovider/add?a={a}&b={b}", Integer.class, a, b);
     }
 
-
-    @RequestMapping(value = "/sayhei",
-        produces = { "application/json" }, 
-        method = RequestMethod.GET)
-    public String sayHei( @RequestHeader(value="name", required=true) String name){
-
-        return userHconsumerDelegate.sayHei(name);
+    public String sayHei(String name){
+        RestTemplate restTemplate = RestTemplateBuilder.create();
+        String microserviceName = "hprovider";
+        String prefix = "cse://" + microserviceName;
+        CseContext.getInstance().getConsumerProviderManager().setTransport(microserviceName, "rest");
+        return restTemplate.getForObject(prefix + "/hprovider/sayhei?name={name}", String.class, name);
     }
 
-
-    @RequestMapping(value = "/sayhello/{name}",
-        produces = { "application/json" }, 
-        method = RequestMethod.POST)
-    public String sayHello( @PathVariable("name") String name){
-
-        return userHconsumerDelegate.sayHello(name);
+    public String sayHello(String name){
+        RestTemplate restTemplate = RestTemplateBuilder.create();
+        String microserviceName = "hprovider";
+        String prefix = "cse://" + microserviceName;
+        CseContext.getInstance().getConsumerProviderManager().setTransport(microserviceName, "rest");
+        return restTemplate.postForObject(prefix + "/hprovider/sayhello/"+name, null, String.class);
     }
 
-
-    @RequestMapping(value = "/sayhi",
-        produces = { "application/json" }, 
-        method = RequestMethod.GET)
-    public String sayHi( @RequestParam(value = "name", required = true) String name){
-
-        return userHconsumerDelegate.sayHi(name);
+    public String sayHi(String name){
+        RestTemplate restTemplate = RestTemplateBuilder.create();
+        String microserviceName = "hprovider";
+        String prefix = "cse://" + microserviceName;
+        CseContext.getInstance().getConsumerProviderManager().setTransport(microserviceName, "rest");
+        return restTemplate.getForObject(prefix + "/hprovider/sayhi?name={name}", String.class, name);
     }
-
-
-    @RequestMapping(value = "/saysomething",
-        produces = { "application/json" }, 
-        method = RequestMethod.POST)
-    public String saySomething( @RequestParam(value = "prefix", required = true) String prefix, @RequestBody Person user){
-
-        return userHconsumerDelegate.saySomething(prefix, user);
+    
+    public String saySomething(String pre, Person user){
+        RestTemplate restTemplate = RestTemplateBuilder.create();
+        String microserviceName = "hprovider";
+        String prefix = "cse://" + microserviceName;
+        CseContext.getInstance().getConsumerProviderManager().setTransport(microserviceName, "rest");
+        return restTemplate.postForObject(prefix + "/hprovider/saysomething?prefix={pre}", user, String.class, pre);
     }
-
 }
